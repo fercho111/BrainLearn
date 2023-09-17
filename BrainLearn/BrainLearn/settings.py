daec6d1646695dbafe8da386e4ed3ad2d05e8ba1
josedjaykv <josedjaykv@gmail.com>
2023-09-17 18:55:04 -0500

    Configurar CORS en nuestra API

--- BrainLearn/BrainLearn/settings.py
+++ BrainLearn/BrainLearn/settings.py
@@ -52,6 +52,7 @@ TOKEN_EXPIRED_AFTER_SECONDS = 900
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'BrainLearn.urls'
@@ -133,6 +133,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [    
+    "http://localhost:3000"    
+]
+
+# Difinimos cual es el puerto del cual podemos recibir peticiones
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
