b511955abda7e9e710ab99e71dce9e7326ded3b4
josedjaykv <josedjaykv@gmail.com>
2023-10-01 23:02:56 -0500

    Usuario Ya registrado

--- BrainLearn/BrainLearn/settings.py
+++ BrainLearn/BrainLearn/settings.py
@@ -98,6 +98,16 @@ DATABASES = {
 
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'brainlearn',
+#        'USER': 'postgres',
+#        'PASSWORD': 'admin',        
+#        'HOST': '127.0.0.1',
+#        'DATABASE_PORT':'5432',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
