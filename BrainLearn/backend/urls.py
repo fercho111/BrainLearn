4da1cbf9cc9e733c9f2a449c4efbb1812c243111
Fer <ferchogomez545@gmail.com>
2023-10-20 10:44:05 -0500

    agregado PUT en ruta /cartas/actualizar

--- BrainLearn/backend/urls.py
+++ BrainLearn/backend/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, UserLogoutView, cartas, mazos
+from .views import UserRegisterView, UserLoginView, UserLogoutView, cartas, mazos, actualizar_carta
 
 urlpatterns = [
     path('mazos/', mazos, name='mazos'),
+    path('cartas/actualizar/<int:card_id>/', actualizar_carta, name='cartas'),
     path('cartas/', cartas, name='cartas'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
--- BrainLearn/backend/views.py
+++ BrainLearn/backend/views.py
@@ -1,5 +1,4 @@
 # Create your views here.
-from django.shortcuts import render
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -34,12 +33,11 @@ def mazos(request):
 
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT'])
 def cartas(request):
     if request.method == "GET":
         if request.user is None:
             return Response({"message": "No se han proporcionado credenciales"}, status=status.HTTP_401_UNAUTHORIZED)
-
         deck_name = request.query_params.get('deck_name')
         if not deck_name:
             return Response({"error": "El nombre del mazo no ha sido proporcionado"}, status=status.HTTP_400_BAD_REQUEST)
@@ -66,6 +64,36 @@ def cartas(request):
             serializer.save(deck=mazo)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def actualizar_carta(request, card_id):
+    if request.method == "PUT":
+        # Check if the user is authenticated
+        if request.user is None:
+            return Response({"message": "No se han proporcionado credenciales"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Get the card with the specified ID
+        try:
+            card = Card.objects.get(id=card_id)
+        except Card.DoesNotExist:
+            return Response({"error": "La tarjeta no existe"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Check if the card's deck belongs to the authenticated user
+        if card.deck.user != request.user:
+            return Response({"error": "No tiene permiso para actualizar esta tarjeta"}, status=status.HTTP_403_FORBIDDEN)
+
+        # Extract the new rating from the request data
+        new_rating = request.data.get("rating")
+
+        # Check that the new_rating is within the valid range (0 to 10)
+        if new_rating is not None and (new_rating < 0 or new_rating > 10):
+            return Response({"error": "La calificación debe estar en el rango de 0 a 10"}, status=status.HTTP_400_BAD_REQUEST)
+
+        card.rating = new_rating
+        card.save()
+
+        return Response({"message": "La tarjeta se ha actualizado correctamente"}, status=status.HTTP_200_OK)
 
 
 # esto tambien hay que entender como funciona
