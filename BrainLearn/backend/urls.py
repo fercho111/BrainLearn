fbbe7cf794c4b567422c3f25569316c5922de3fb
josedjaykv <josedjaykv@gmail.com>
2023-10-27 16:42:51 -0500

    Organizando urls.py

--- BrainLearn/backend/urls.py
+++ BrainLearn/backend/urls.py
@@ -4,10 +4,16 @@ from .views import UserRegisterView, UserLoginView, UserLogoutView, cartas, mazo
 
 urlpatterns = [
     path('deckList/', mazos, name='mazos'),
+
+    # Mazos
     path('mazos/', mazos, name='mazos_alt'),
     path('mazos/<int:mazo_id>/', actualizar_eliminar_mazo, name='mazos'),
-    path('cartas/<int:card_id>/', actualizar_eliminar_carta, name='cartas'),    
+
+    # Cartas
     path('cartas/', cartas, name='cartas'),
+    path('cartas/<int:card_id>/', actualizar_eliminar_carta, name='cartas'),    
+
+    # Usuarios
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
